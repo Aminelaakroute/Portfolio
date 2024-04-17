c7c866f731114b1292f0114bb8d97075ca77c778
AmineLke <amine.laakroute@etu.uae.ac.ma>
2024-04-17 03:42:39 +0100

    Hero 1.1

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
   return (
     <div className='container'>
       <Header />
-      <div className='divider'/>
+  
       <Hero />
       <div className='divider'/>
       <Main />
--- src/components/2-hero/Hero.jsx
+++ src/components/2-hero/Hero.jsx
@@ -3,9 +3,34 @@ import './Hero.css'
 
 const Hero = () => {
   return (
-    <div>
-      HERO 
-    </div>
+    <section className='hero flex'>
+      <div className='left-section'>
+      
+       <div className="parent-avatar flex">
+        <img src="./AMINE.png" className="avatar" />
+        <div className='icon-verified'/>
+       </div>
+
+       <h1 className='title'>
+        Full stack developer (M.E.R.N)<br />
+        Cyber Security Student
+       </h1>
+       <p className='subtitle'>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ratione suscipit a velit necessitatibus aliquid. Esse, debitis error maiores officia repudiandae accusantium iste cupiditate architecto, impedit a suscipit eius atque perspiciatis?
+       </p>
+
+       <div className="all-icons flex ">
+        <div className="icon icon-facebook2" ></div>
+        <div className="icon icon-instagram"></div>
+        <div className="icon icon-github"></div>
+        <div className="icon icon-linkedin1"></div>
+       </div>
+
+      </div>
+      <div className='Right-section animation border'>right
+
+      </div>
+    </section>
   )
 }
 export default Hero;
